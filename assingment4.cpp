fa7d77e81a9ad63f6d87f85ef16f95533deb20b5
RISHABH MISHRA <141563784+RishabhLpu@users.noreply.github.com>
2024-08-06 20:32:46 +0530

    assingment4.cpp

---
+++ assingment4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int ssquareRoot(int n)
+{
+    return sqrt(n);
+}
+int main() {
+int s=9;
+int r=ssquareRoot(s);
+cout<<"Squareroot of the  number are "<<r;
+    return 0;
+}
